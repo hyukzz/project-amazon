0b489b6066d002d4fbcef7f204b28478158b4b7c
hyukzz <ri_chard@naver.com>
2022-09-20 21:36:16 +0900

    [#2]Design: 헤더 디자인 완성

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { ShoppingBasket } from "@material-ui/icons";
 
 function Header() {
 	return (
@@ -11,22 +13,30 @@ function Header() {
 
 			<div className="header_search">
 				<input className="header_searchInput" type="" />
+				<SearchIcon className="header_searchIcon" />
 			</div>
 
 			<div className="header_nav">
 				<div className="header_option">
 					<span className="header_option_firstLine">안녕하세요</span>
-					<span className="header_option_secondtLine">로그인하기</span>
+					<span className="header_option_secondLine">로그인하기</span>
 				</div>
 
 				<div className="header_option">
 					<span className="header_option_firstLine">돌아가기</span>
-					<span className="header_option_secondtLine">주문하기</span>
+					<span className="header_option_secondLine">주문하기</span>
 				</div>
 
 				<div className="header_option">
 					<span className="header_option_firstLine">반가워요</span>
-					<span className="header_option_secondtLine">구독과좋아요</span>
+					<span className="header_option_secondLine">구독과좋아요</span>
+				</div>
+
+				<div className="header_option">
+					<span className="header_option_basket">
+						<ShoppingBasket />
+						<span className="header_option_basketCount">0</span>
+					</span>
 				</div>
 			</div>
 		</div>
