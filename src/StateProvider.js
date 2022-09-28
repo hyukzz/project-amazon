918246aa777ba3cdfd95553b81aee8865bf1ef28
hyukzz <ri_chard@naver.com>
2022-09-28 15:02:25 +0900

    배포 테스트

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -12,3 +12,4 @@ export const StateProvider = ({ reducer, initialState, children }) => {
 // 앱을 래핑하고 데이터 레이어를 제공
 
 export const useStateValue = () => useContext(StateContext);
+//Context를 통해 전역적 데이터를 공유할 때 효율적으로 사용가능하다.
